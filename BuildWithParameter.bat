6e0e447e720af3c430d1e66533350dc4a6a40af7
aFei224 <2721252071@qq.com>
2021-01-17 21:27:16 +0800

    +CANSHU

--- BuildWithParameter.bat
+++ BuildWithParameter.bat
@@ -1,11 +1,1 @@
-REM UNITY程序的路径
-SET UNITY_PATH="C:\Program Files\Unity\Hub\Editor\2019.4.18f1c1\Editor\Unity.exe"
- 
-REM 游戏程序路径
-SET PROJECT_PATH="F:\U3D Project\BeanFire"
- 
-REM 在Unity中构建apk
-%UNITY_PATH% -projectPath %PROJECT_PATH%  -executeMethod ProjectBuild.BuildForAndroid -logFile build.log -quit
- 
-echo "Apk生成完毕"
-PAUSE
+%1 -projectPath %2 -quit -batchmode -executeMethod ProjectBuild.BuildForAndroid -logFile build.log
